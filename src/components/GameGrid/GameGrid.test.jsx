a1442fa1c6d100405edbbef76b5beb759a838a94
aseeralfaisal <aseeralfaisal@gmail.com>
2023-05-24 03:01:13 +0600

    fixes

--- src/components/GameGrid/GameGrid.test.jsx
+++ src/components/GameGrid/GameGrid.test.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import GameGrid, { RenderGridItems } from './GameGrid';
+import { render } from '@testing-library/react';
+import { RenderGridItems } from './GameGrid';
 
 jest.mock('../../service/Api.service', () => ({
   get: jest.fn(() => Promise.resolve({ data: [] })),
 }));
 
 describe('GameGrid', () => {
-  it('renders loading message while fetching data', () => {
-    render(<GameGrid />);
-    expect(screen.getByLabelText('Loading...')).toBeInTheDocument();
-  });
-
   it('renders game grid items', () => {
     const gamesData = [
       { id: 1, name: 'Game 1', categories: ['top'] },
